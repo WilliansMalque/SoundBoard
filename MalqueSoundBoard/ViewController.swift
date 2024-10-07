0387d79b79190a95127fcd787d98b6e1c5a09ca5
Willians Malque <willians.malque@tecsup.edu.pe>
2024-10-07 16:59:39 -0500

    Initial Commit

---
+++ MalqueSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MalqueSoundBoard
+//
+//  Created by Willians Malque on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
